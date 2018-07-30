e12c3261ffacd5d8db904eb9bedd0e46bb7a0209
simperLv <593324988@qq.com>
2018-07-30 17:34:32 +0800

    java8 中的stream特性，尝试通过流的形式遍历集合。

---
+++ src/main/java/com/lv/model/Person.java
@@ -0,0 +1,26 @@
+package com.lv.model;
+
+/**
+ * Created by simperLv
+ * on 2018/07/30 16:32
+ */
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getInfo(){
+        return name + "(" + age + ")";
+    }
+}
--- src/main/java/com/lv/util/StreamTest.java
+++ src/main/java/com/lv/util/StreamTest.java
@@ -1,107 +1,36 @@
 package com.lv.util;
 
-import java.io.File;
-import java.text.DecimalFormat;
+import com.lv.model.Person;
+
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.function.Predicate;
 
 /**
- * Created by lvsr
- * on 2018/07/25 17:57
+ * Created by simperLv
+ * on 2018/07/30 16:25
  */
 public class StreamTest {
-    public static void main(String[] args) {
-       /*File f = new File("E:\\test.pdf");
-       if (f.exists() && f.isFile()){
-           System.out.print(f.length());
-       }else{
-           System.out.print("do not find");
-       }*/
-        File file = new File("E:\\开题报告");
-        List<Map<String,String>> list = getListFiles("E:\\开题报告", "", true);
-        for (Map<String,String> fileName : list) {
-            System.out.println(fileName);
-        }
-        System.out.println(list.size());
-        try {
-            System.out.println(FormetFileSize(getFileSize(file)));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    public static void main(String[] args){
+        List<Person> people = new ArrayList<Person>();
+        people.add(new Person("lv",22));
+        people.add(new Person("gou",23));
+        people.add(new Person("yueer",21));
 
-    public static List<Map<String,String>> getListFiles(String path,String suffix,boolean isdepth){
-        List<Map<String,String>> lstFileNames = new ArrayList<Map<String,String>>();
-        //List<Long> lstFileSizes = new ArrayList<Long>();
-        File file = new File(path);
-        return StreamTest.listFile(lstFileNames,file,suffix,isdepth);
+        Predicate<Person> pred = (p) -> p.getAge() > 22;
+        displayPeople(people,pred);
     }
-    private static List<Map<String,String>> listFile(List<Map<String,String>> lstFileNames,File f,String suffix,boolean isdepth){
-        //若是目录，采用递归的方法遍历子目录
-        File[] t = f.listFiles();
-        if(f.isDirectory()){
-            for(int i=0;i<t.length;i++){
-                if(isdepth || t[i].isFile()){
-                    listFile(lstFileNames,t[i],suffix,isdepth);
-                }
+    public static void displayPeople(List<Person> people,Predicate<Person> pred){
+        System.out.print("selected:");
+        /*people.forEach(p -> {
+            if (pred.test(p)){
+                System.out.print(p.getName());
             }
-        }else {
-            String filePath = f.getAbsolutePath();
-            long fileSize = f.length();
-            Map<String,String> map = new HashMap<String,String>();
-            int begIndex = filePath.lastIndexOf("."); // 最后一个.(即后缀名前面的.)的索引
-            if (!suffix.equals("")) {
-                String tempsuffix = "";
-                if (begIndex != -1) {
-                    tempsuffix = filePath.substring(begIndex + 1,
-                            filePath.length());
-                    if (tempsuffix.equals(suffix)) {
-                        String fileSizes = FormetFileSize(fileSize);
-                        String filePaths = filePath.substring(begIndex+1,filePath.length());
-                        map.put(filePaths,fileSizes);
-                        lstFileNames.add(map);
-                    }
-                }
-
-            }else{
-                String filePaths = filePath.substring(begIndex+1,filePath.length());
-                String fileSizes = FormetFileSize(fileSize);
-                map.put(filePaths,fileSizes);
-                lstFileNames.add(map);
-            }
-        }
-        return lstFileNames;
+        });*/
+        people.stream()
+                .filter(pred)
+                .forEach(p ->System.out.println(p.getName()));
+        people.stream()
+                .forEach(p -> System.out.println(p.getName() + "的年龄是:" + p.getAge()));
     }
-
-    // 取得文件夹大小
-    public static long getFileSize(File f) throws Exception{
-        long size = 0;
-        File flist[] = f.listFiles();
-        for (int i = 0; i < flist.length; i++) {
-            if (flist[i].isDirectory()) {
-                size = size + getFileSize(flist[i]);
-            } else {
-                size = size + flist[i].length();
-            }
-        }
-        return size;
-    }
-
-    public static String FormetFileSize(long fileS) {// 转换文件大小
-        DecimalFormat df = new DecimalFormat("#.00");
-        String fileSizeString = "";
-        if (fileS < 1024) {
-            fileSizeString = df.format((double) fileS) + "B";
-        } else if (fileS < 1048576) {
-            fileSizeString = df.format((double) fileS / 1024) + "K";
-        } else if (fileS < 1073741824) {
-            fileSizeString = df.format((double) fileS / 1048576) + "M";
-        } else {
-            fileSizeString = df.format((double) fileS / 1073741824) + "G";
-        }
-        return fileSizeString;
-    }
-
 }
